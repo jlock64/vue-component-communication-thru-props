9626d5c367bb2c1e9c3453959b3bd492f0c7cac5
Justin Lock <jlock@TRHC.com>
2022-06-08 20:16:13 -0400

    using $emits to send data back from child to parent component to update props

--- src/components/FriendContact.vue
+++ src/components/FriendContact.vue
@@ -1,6 +1,6 @@
 <template>
   <li>
-    <h2>{{ name }} {{ friendIsFavorite ? '(Favorite)' : ''}}</h2>
+    <h2>{{ name }} {{ isFavorite ? '(Favorite)' : ''}}</h2>
     <button @click="toggleFavorite">Toggle Favorite</button>
     <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
     <ul v-if="detailsAreVisible">
@@ -24,6 +24,10 @@ export default {
   //   'emailAddress',
   // ],
   props: {
+    id: {
+      type: String,
+      required: true
+    },
     name: {
       type: String,
       required: true
@@ -45,10 +49,20 @@ export default {
       // }
     }
 },
+  emits: ['toggle-favorite'],
+  // emits: {
+  //   'toggle-favorite': function(id) {
+  //     if (id) {
+  //       return true;
+  //     } else {
+  //       console.warn('Id is missing')
+  //       return false;
+  //     }
+  //   }
+  // },
   data() {
     return {
       detailsAreVisible: false,
-      friendIsFavorite: this.isFavorite
     }
   },
   methods: {
@@ -56,7 +70,7 @@ export default {
       this.detailsAreVisible = !this.detailsAreVisible;
     },
     toggleFavorite() {
-      this.friendIsFavorite = !this.friendIsFavorite;
+      this.$emit('toggle-favorite', this.id);
     }
   },
 }
